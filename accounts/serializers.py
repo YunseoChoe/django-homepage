eff1ac9364792dce4933d85d4b0b353752bb21cd
choeyunseo <choeyunseo4060@naver.com>
2024-02-13 00:24:43 +0900

    Add JWT token

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,13 @@
-# 필요한 모듈 및 클래스를 임포트합니다.
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
 from .models import CustomUser
 
-# 회원가입을 위한 시리얼라이저를 정의합니다.
 class UserSerializers(serializers.ModelSerializer):
-    # Meta 클래스에서 모델과 필드를 설정합니다.
     class Meta:
         model = CustomUser
         fields = ['email', 'password', 'username', 'age', 'height', 'weight']
         # 비밀번호 필드는 쓰기 전용(write_only)으로 설정합니다.
         extra_kwargs = {'password': {'write_only': True}}
 
-    # 사용자 생성 메서드를 오버라이드합니다.
     def create(self, validated_data):
         # 비밀번호를 추출합니다.
         password = validated_data.pop('password')
@@ -22,13 +17,5 @@ class UserSerializers(serializers.ModelSerializer):
         user.set_password(password)
         # 사용자를 저장합니다.
         user.save()
-
-        # 회원가입 후에 사용자를 위한 리프레시 토큰과 액세스 토큰을 생성합니다.
-        refresh = RefreshToken.for_user(user)
-        # 생성된 토큰들을 JSON 형식으로 구성하여 반환합니다.
-        tokens = {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }
-
-        return tokens
+        # 사용자 객체를 반환합니다.
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate # login했을 때 데이터가 db에 있는 지 확인
 from rest_framework import generics
 from .serializers import UserSerializers
-
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -17,19 +17,22 @@ class UserCreateAPIView(generics.CreateAPIView):
 
 # 로그인
 class LoginAPIView(APIView):
-    # POST 메서드를 정의합니다.
     def post(self, request):
-        # POST 요청에서 'username'과 'password' 데이터를 가져옵니다.
         username = request.data.get('username')
         password = request.data.get('password')
 
-        # authenticate 함수를 사용하여 사용자를 인증합니다. (해당 사용자가 존재하고 비밀번호가 올바른지 확인)
+        # 사용자 인증
         user = authenticate(username=username, password=password)
         
-        # 사용자가 존재하지 않으면 에러 응답을 반환합니다.
         if user is None:
             return Response({'error': 'Invalid username or password.'}, status=status.HTTP_400_BAD_REQUEST)
         
-        # 사용자가 존재하면 "success login!" 메시지를 포함한 응답을 반환합니다.
-        response = HttpResponse("success login!")
-        return response
+        # 토큰 발급
+        refresh = RefreshToken.for_user(user) # Simple JWT의 RefreshToken.for_user() 메서드를 사용
+        
+        # 응답에 토큰 포함하여 반환
+        return Response({
+            'message': 'Login successful!',
+            'refresh_token': str(refresh),                  # 리프레시 토큰
+            'access_token': str(refresh.access_token),      # 액세스 토큰
+        }, status=status.HTTP_200_OK)
