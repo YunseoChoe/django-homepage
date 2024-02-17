279f85a6de3e6204e3c405fe63669edc63425981
choeyunseo <choeyunseo4060@naver.com>
2024-02-17 14:12:23 +0900

    Add board/views and urls

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import BoardCreateAPIView
+from .views import BoardUpdateAPIView
+from .views import BoardDeleteAPIView
+from .views import BoardReadAPIView
 
 urlpatterns = [
-    path('boardcreate/', BoardCreateAPIView.as_view()),
+    path('board_create/', BoardCreateAPIView.as_view()),
+    path('board_update/<int:pk>/', BoardUpdateAPIView.as_view()),
+    path('board_delete/<int:pk>/', BoardDeleteAPIView.as_view()),
+    path('board_read/<int:pk>/', BoardReadAPIView.as_view()),
 ]
